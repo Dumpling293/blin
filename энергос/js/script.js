ef0f8fe109b8af1999a0ee82504988f7a164bd0b
Dumpling293 <58853217+Dumpling293@users.noreply.github.com>
2023-06-05 12:15:59 +0400

    Add files via upload

---
+++ энергос/js/script.js
@@ -0,0 +1,17 @@
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+  const button = dropdown.querySelector('.dropdown-content');
+
+  button.addEventListener('click', () => {
+    dropdowns.forEach(d => {
+      if (d !== dropdown) {
+        d.classList.remove('show');
+      }
+    });
+    dropdown.classList.toggle('show');
+  });
+});
+
+let yearSelect = document.querySelector('select');
+let selectedYear = yearSelect.value;
